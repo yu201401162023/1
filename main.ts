6cb0d5adf0a7aa410d1f1c6ed74776c036d3eba4
yu201401162023 <133978542+yu201401162023@users.noreply.github.com>
2023-08-22 07:56:41 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,52 @@
-let 造型 = 0
+input.onGesture(Gesture.ScreenDown, function () {
+    if (模式 == 1) {
+        模式2造型 = randint(0, 1)
+        if (模式2造型 == 0) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+        }
+        if (模式2造型 == 1) {
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+                `)
+        }
+    }
+})
 input.onButtonPressed(Button.A, function () {
-    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+    if (模式 == 0) {
+        模式 += 1
+        basic.showString("ROCK-PEPER-SCISSORS")
+    }
+    if (模式 == 1) {
+        模式 += 1
+        basic.showString("BLACK AND WHITE")
+    }
+    if (模式 == 2) {
+        模式 += -1
+        basic.showString("DICE")
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (模式 == 0) {
+        basic.showString("FLICK THE YOUR MICRO:BIT!")
+    }
+    if (模式 == 1) {
+        basic.showString("PLACE YOUR MICRO:BIT FACE DOWN!")
+    }
+    if (模式 == 2) {
+        basic.showString("FLICK THE YOUR MICRO:BIT!")
+    }
+})
+input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -9,36 +55,108 @@ input.onButtonPressed(Button.A, function () {
         . . . . .
         `)
 })
-input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Happy)
-})
 input.onGesture(Gesture.ThreeG, function () {
-    造型 = randint(1, 3)
-    if (造型 == 1) {
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # # # .
-            . # # # .
-            . . . . .
-            `)
+    if (模式 == 0) {
+        模式1造型 = randint(0, 2)
+        if (模式1造型 == 0) {
+            basic.showLeds(`
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+                . . . . .
+                `)
+        }
+        if (模式1造型 == 1) {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+        }
+        if (模式1造型 == 2) {
+            basic.showLeds(`
+                # # # # #
+                # . . . #
+                # . . . #
+                # . . . #
+                # # # # #
+                `)
+        }
     }
-    if (造型 == 2) {
-        basic.showLeds(`
-            # # . . #
-            # # . # .
-            . . # . .
-            # # . # .
-            # # . . #
-            `)
-    }
-    if (造型 == 3) {
-        basic.showLeds(`
-            # # # # #
-            # . . . #
-            # . . . #
-            # . . . #
-            # # # # #
-            `)
+    if (模式 == 2) {
+        模式3造型 = randint(0, 2)
+        if (模式3造型 == 0) {
+            basic.showLeds(`
+                . . . # .
+                . . . # .
+                . . . # .
+                . . . # .
+                . . . # .
+                `)
+        }
+        if (模式3造型 == 1) {
+            basic.showLeds(`
+                . # # # .
+                . . . # .
+                . # # # .
+                . # . . .
+                . # # # .
+                `)
+        }
+        if (模式3造型 == 2) {
+            basic.showLeds(`
+                . # # # .
+                . . . # .
+                . # # # .
+                . . . # .
+                . # # # .
+                `)
+        }
+        if (模式3造型 == 3) {
+            basic.showLeds(`
+                . # . # .
+                . # . # .
+                . # # # .
+                . . . # .
+                . . . # .
+                `)
+        }
+        if (模式3造型 == 4) {
+            basic.showLeds(`
+                . # # # .
+                . # . . .
+                . # # # .
+                . . . # .
+                . # # # .
+                `)
+        }
+        if (模式3造型 == 5) {
+            basic.showLeds(`
+                . # # # .
+                . # . . .
+                . # # # .
+                . # . # .
+                . # # # .
+                `)
+        }
     }
 })
+let 模式3造型 = 0
+let 模式2造型 = 0
+let 模式1造型 = 0
+let 模式 = 0
+模式 = 0
+模式1造型 = 0
+模式2造型 = 0
+if (模式 == 0) {
+    basic.showString("ROCK-PEPER-SCISSORS")
+}
+if (模式 == 1) {
+    basic.showString("BLACK AND WHITE")
+}
+if (模式 == 2) {
+    basic.showString("DICE")
+}
